20bcc517f0c90b04780c354b2c249176013c8b85
lu1234566 <barcelar34@gmail.com>
2026-07-17 01:48:43 -0300

    Add the technical-agent miniboss

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -860,12 +860,13 @@
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
 #define TRAINER_ARAUNA_SCOUT_NILO           855
+#define TRAINER_ARAUNA_TECH_AGENT           856
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT_EMERALD     856
+#define TRAINERS_COUNT_EMERALD     857
 #define MAX_TRAINERS_COUNT_EMERALD 864
 
 #if IS_FRLG
