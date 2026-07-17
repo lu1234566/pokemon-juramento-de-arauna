97bf45d2dd6364280eb336545132d542d669e692
lu1234566 <barcelar34@gmail.com>
2026-07-17 01:23:11 -0300

    Register the bilingual Mist Route text bank

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1742,6 +1742,7 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/text/birch_speech.inc"
 	.include "data/text/arauna/map_lab.inc"
 	.include "data/text/arauna/opening.inc"
+	.include "data/text/arauna/route.inc"
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
