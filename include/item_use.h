bbb36c01f0b90d3292bcfc07c9c0c5263f9d12c7
lu1234566 <barcelar34@gmail.com>
2026-07-20 19:27:22 -0300

    feat(field-tools): expose Tide Board bag behavior

--- include/item_use.h
+++ include/item_use.h
@@ -39,6 +39,7 @@ void ItemUseOutOfBattle_Honey(u8 taskId);
 void ItemUseOutOfBattle_CannotUse(u8 taskId);
 void ItemUseOutOfBattle_ExpShare(u8 taskId);
 void ItemUseOutOfBattle_AraunaNotebook(u8 taskId);
+void ItemUseOutOfBattle_AraunaBoard(u8 taskId);
 void ItemUseInBattle_BagMenu(u8 taskId);
 void ItemUseInBattle_PartyMenu(u8 taskId);
 void ItemUseInBattle_PartyMenuChooseMove(u8 taskId);
