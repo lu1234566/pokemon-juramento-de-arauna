4bc20f75ba2a71b71f5f70646f86b0f969f6873f
lu1234566 <barcelar34@gmail.com>
2026-08-19 17:36:53 -0300

    Enable English Ruinas and Memorial overlay

--- scripts/build_arauna.sh
+++ scripts/build_arauna.sh
@@ -28,6 +28,10 @@ overlay_files=(
     "data/maps/MtChimney/scripts.inc"
     "data/maps/FortreeCity/scripts.inc"
     "data/maps/FortreeCity_Gym/scripts.inc"
+    "data/maps/MeteorFalls_1F_1R/scripts.inc"
+    "data/maps/MtPyre_Summit/scripts.inc"
+    "src/data/items.h"
+    "src/data/text/item_descriptions.h"
 )
 overlay_backup_dir="$(mktemp -d)"
 
@@ -52,6 +56,7 @@ python3 scripts/render_petalburg_woods_surface.py --in-place
 python3 scripts/render_route119_ciro_surface_en.py --in-place
 python3 scripts/render_mt_chimney_surface.py --in-place
 python3 scripts/render_mata_do_meio_lidia_en.py --in-place
+python3 scripts/render_ruins_memorial_en.py --in-place
 
 cpp="${CPP:-arm-none-eabi-cpp}"
 
