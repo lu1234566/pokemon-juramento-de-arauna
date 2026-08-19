0c2fcf9c885279a1f6f76dd0adca5b4c63ca7b46
lu1234566 <barcelar34@gmail.com>
2026-08-19 19:57:38 -0300

    Wire Route 102 and Pampa da Espera into English build

--- scripts/build_arauna.sh
+++ scripts/build_arauna.sh
@@ -26,8 +26,10 @@ overlay_files=(
     "src/data/trainers.h"
     "data/maps/LittlerootTown/scripts.inc"
     "data/maps/Route101/scripts.inc"
+    "data/maps/Route102/scripts.inc"
     "data/maps/Route103/scripts.inc"
     "data/maps/OldaleTown/scripts.inc"
+    "data/maps/PetalburgCity/scripts.inc"
     "data/maps/LittlerootTown_BrendansHouse_1F/scripts.inc"
     "data/maps/LittlerootTown_MaysHouse_1F/scripts.inc"
     "data/maps/LittlerootTown_MaysHouse_2F/scripts.inc"
@@ -95,6 +97,7 @@ python3 scripts/render_vila_amanhecer_houses_en_checked.py --in-place
 python3 scripts/render_anahi_lab_en_checked.py --in-place
 python3 scripts/render_route103_ciro_en_checked.py --in-place
 python3 scripts/render_vila_da_passagem_en.py --in-place
+python3 scripts/render_route102_pampa_en_checked.py --in-place
 python3 scripts/render_petalburg_woods_surface.py --in-place
 python3 scripts/render_route119_ciro_surface_en.py --in-place
 python3 scripts/render_baia_luzes_ciro_en.py --in-place
