a45e27a75ceb7ac7bf901f4a4332a09f1f39ce90
lu1234566 <barcelar34@gmail.com>
2026-08-20 00:55:00 -0300

    Wire Serra and Porto interiors into English build [skip ci]

--- scripts/build_arauna.sh
+++ scripts/build_arauna.sh
@@ -39,11 +39,17 @@ overlay_files=(
     "data/maps/Route104/scripts.inc"
     "data/maps/RustboroCity/scripts.inc"
     "data/maps/RustboroCity_Gym/scripts.inc"
+    "data/maps/RustboroCity_DevonCorp_1F/scripts.inc"
+    "data/maps/RustboroCity_DevonCorp_2F/scripts.inc"
     "data/maps/RustboroCity_DevonCorp_3F/scripts.inc"
+    "data/maps/RustboroCity_CuttersHouse/scripts.inc"
+    "data/maps/RustboroCity_PokemonSchool/scripts.inc"
     "data/maps/Route116/scripts.inc"
     "data/maps/RusturfTunnel/scripts.inc"
     "data/maps/DewfordTown/scripts.inc"
     "data/maps/DewfordTown_Gym/scripts.inc"
+    "data/maps/DewfordTown_House2/scripts.inc"
+    "data/maps/GraniteCave_StevensRoom/scripts.inc"
     "data/maps/PetalburgWoods/scripts.inc"
     "data/maps/Route119/scripts.inc"
     "data/maps/Route120/scripts.inc"
@@ -112,6 +118,7 @@ python3 scripts/render_pampa_elias_gym_core_en_checked.py --in-place
 python3 scripts/render_pampa_gym_rooms_en_checked.py --in-place
 python3 scripts/render_val_house_en_checked.py --in-place
 python3 scripts/render_serra_uivo_porto_redes_en.py --in-place
+python3 scripts/render_serra_uivo_porto_redes_interiors_en.py --in-place
 python3 scripts/render_petalburg_woods_surface.py --in-place
 python3 scripts/render_route119_ciro_surface_en.py --in-place
 python3 scripts/render_baia_luzes_ciro_en.py --in-place
