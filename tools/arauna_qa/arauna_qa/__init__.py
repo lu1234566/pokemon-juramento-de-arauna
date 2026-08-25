95535f6291ad46a1362d1129968c889a19e9ef76
lu1234566 <barcelar34@gmail.com>
2026-08-25 14:43:18 -0300

    Export scenario reporting API [skip ci]

--- tools/arauna_qa/arauna_qa/__init__.py
+++ tools/arauna_qa/arauna_qa/__init__.py
@@ -9,6 +9,7 @@ from .objects import ObjectEventReader, ObjectEventState
 from .party import PartyMonState, PartyReader, PartySnapshot
 from .protocol import BridgeInfo, MgbaBridge, ProtocolError, key_mask
 from .repo_map import CollisionGrid, LayoutDefinition, MapCell, MapDefinition, MapIssue, RepoMapIndex
+from .reporting import ScenarioBundleResult, ScenarioReporter, safe_artifact_name
 from .scenario import ScenarioResult, ScenarioRunner, ScenarioStepResult
 from .state import AraunaState, AraunaStateReader
 from .symbols import Symbol, SymbolTable
@@ -25,7 +26,7 @@ __all__ = [
     "LayoutDefinition", "MapCell", "MapDefinition", "MapIssue", "MapTransition",
     "MoveAdvice", "MoveInfo", "MoveResult", "MgbaBridge", "Navigator", "NpcInteractor",
     "ObjectEventReader", "ObjectEventState", "PartyMonState", "PartyReader", "PartySnapshot",
-    "ProtocolError", "RepoMapIndex", "ScenarioResult", "ScenarioRunner", "ScenarioStepResult",
+    "ProtocolError", "RepoMapIndex", "ScenarioBundleResult", "ScenarioReporter", "ScenarioResult", "ScenarioRunner", "ScenarioStepResult",
     "StateWatchdog", "Symbol", "SymbolTable", "TransitionResult", "WatchEvent", "WalkResult",
-    "WorldNavigationResult", "WorldNavigator", "WorldRoute", "WorldRouter", "key_mask",
+    "WorldNavigationResult", "WorldNavigator", "WorldRoute", "WorldRouter", "key_mask", "safe_artifact_name",
 ]
